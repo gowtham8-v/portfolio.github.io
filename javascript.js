b44d10d3db89935b35375f7db0ee165a75e0a5b5
gowtham8-v <gowthamvenkat306@gmail.com>
2025-08-16 11:33:01 +0530

    created

---
+++ javascript.js
@@ -0,0 +1,18 @@
+    function abt(){
+    document.querySelector('.about').scrollIntoView({behavior: "smooth"})
+      document.querySelector('.about').style.animation="fade 4s"
+
+    }
+
+    function proj(){
+    document.querySelector('.project-detail').scrollIntoView({behavior: "smooth"})
+    }
+
+    function skil(){
+    document.querySelector('.skills').scrollIntoView({behavior: "smooth"})
+    document.querySelector('.skills').style.animation="fade 5s"
+    }
+
+    function cont(){
+    document.querySelector('.contact').scrollIntoView({behavior: "smooth"})
+    }
